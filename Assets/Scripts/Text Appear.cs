e3918baa1bf37dbb10251f725781ff9b44eea575
Sergelongolous <matosse@sheridancollege.ca>
2026-02-06 17:30:27 -0500

    Attempted to make text appear if you hover the mouse over a sprite (It did not work, this was tricky)

---
+++ Assets/Scripts/Text Appear.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TextAppear : MonoBehaviour
+{
+    public GameObject bird;
+    public GameObject birdText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isHovered = Mouse.current.ReadValueAsObject();
+        if (isHovered)
+        {
+            birdText.SetActive(true);
+        }
+    }
+}
