0c3de3e5969b9fa3593128d3e414ae440ef3412b
Sergelongolous <matosse@sheridancollege.ca>
2026-02-19 15:00:55 -0500

    attempted 2 scripts. One that makes the steam sprite spawn and destroyed with a timer and the other script chages a object's opacity to 0. Also added more details to the overall game

---
+++ Assets/Scripts/Light Button.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightButton : MonoBehaviour
+{
+    public Camera gameCamera;
+    public GameObject lightButton;
+    private bool isPressed = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed == true)
+        {
+            lightButton.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    public void ColorChange()
+    {
+        if (isPressed == true)
+        {
+            Color p = new Color(9, 10, 34, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Steam Button.cs
@@ -0,0 +1,47 @@
+using System.IO.Pipes;
+using UnityEngine;
+
+public class SteamButton : MonoBehaviour
+{
+    public GameObject steamObject;
+
+    public float waitDuration;
+    public float destroyDuration;
+    public float clockDuration;
+
+    private float destroyProgress = 0f;
+    private float waitProgress = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waitProgress += Time.deltaTime;
+        if (waitProgress > waitDuration)
+        {
+            GameObject spawnedObject = Instantiate(steamObject, transform.position, Quaternion.identity);
+
+            SteamButton spawnedsteamObject = steamObject.GetComponent<SteamButton>();
+            spawnedsteamObject.transform.position = transform.position;
+
+            Destroy(spawnedsteamObject, destroyDuration);
+
+            waitProgress = 0f;
+        }
+    }
+
+    public void SteamSpawn()
+    {
+        clockDuration -= Time.deltaTime * 1;
+
+        if (clockDuration <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
